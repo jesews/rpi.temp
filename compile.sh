b7ef9a2f10e219522107f55bb2b550d5f0e918a2
jesews <108303329+jesews@users.noreply.github.com>
2022-06-27 16:15:25 -0500

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -4,4 +4,8 @@ gcc temp.c -o temp
 
 sudo mv ./temp /usr/local/bin/
 
+cd ..
+
+sudo rm -rf rpi.temp/
+
 echo "Script has completed, use 'temp' from any directory to ouput the current temp in Celsius"
